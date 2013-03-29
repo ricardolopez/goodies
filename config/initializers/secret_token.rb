01e6d4279d21d9fd81e51835b1c4c28ed5115adb
Ricardo Lopez <rlopez@mymail.mines.edu>
2013-03-28 23:33:09 -0600

    Adding empty project files along with README

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Goodies::Application.config.secret_token = '8ecff89b7ed953db3a13ce0c5f4bcb2dda76cea3b6222284a3128a9d431f36d25ef438575d1c29afab90950aca48388356d7225a7cb0ace44835ccd43c45fa96'
